93218eaf6aa58ee475262f1be8d7b16c54847083
leopalatto20 <A01665462@tec.mx>
2025-09-12 10:36:16 -0600

    creacion de servicio y modulo de base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DbService } from './db/db.service';
+import { DbModule } from './db/db.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true
-        })
+        }),
+        DbModule
     ],
     controllers: [],
-    providers: []
+    providers: [DbService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,4 @@ async function bootstrap() {
     SwaggerModule.setup('/docs', app, doc);
     await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
