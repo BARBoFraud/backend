bfa1a8766f9867bfdbd760a6f020086aa2f4cede
Leonardo Pérez <leote@Leonardos-MacBook-Air.local>
2025-10-13 20:16:31 -0600

    documentar endpoint de desactivar

--- src/users/dto/deactivate-user.dto.ts
+++ src/users/dto/deactivate-user.dto.ts
@@ -1,3 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class DeactivateUserDto {
+    @ApiProperty({
+        description: 'Contraseña del usuario que va a desactivarse'
+    })
     password: string;
 }
