a93038e695dc9efdc9223ea83e4d75f47ef2234c
Leonardo Pérez <leote@Leonardos-MacBook-Air.local>
2025-10-19 14:03:12 -0600

    arreglar errores de codigo duplicado de sonarcloud

--- src/reports/dto/update-report.dto.ts
+++ src/reports/dto/update-report.dto.ts
@@ -1,19 +1,10 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import {
-    IsBoolean,
-    IsEmail,
-    IsNumber,
-    IsOptional,
-    IsString,
-    IsUrl,
-    MaxLength
-} from 'class-validator';
+import { PartialType } from '@nestjs/swagger';
+import { CreateReportDto } from './create-report.dto';
+import { IsNumber, IsString, IsBoolean, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateReportDto {
-    @ApiProperty({
-        example: 1,
-        description: 'Nueva categoria del reporte'
-    })
+export class UpdateReportDto extends PartialType(CreateReportDto) {
+    @ApiProperty({ example: 1, description: 'Categoria del reporte' })
     @IsNumber()
     categoryId: number;
 
@@ -33,70 +24,7 @@ export class UpdateReportDto {
     @MaxLength(255)
     description: string;
 
-    @ApiPropertyOptional({
-        example: 'www.tennisgratis.com',
-        description: 'Nueva url de la pagina web implicada'
-    })
-    @IsUrl()
-    @IsOptional()
-    url?: string;
-
-    @ApiPropertyOptional({
-        example: 'Tennis Gratis Mx',
-        description: 'Nuevo nombre de la pagina web'
-    })
-    @IsString()
-    @MaxLength(256)
-    @IsOptional()
-    website?: string;
-
-    @ApiPropertyOptional({
-        example: 'Instagram',
-        description: 'Aplicacion en la que ocurrio la estafa'
-    })
-    @IsString()
-    @MaxLength(30)
-    @IsOptional()
-    application?: string;
-
-    @ApiPropertyOptional({
-        example: '5627452471',
-        description: 'Numero telefonico desde el que ocurrio el incidente'
-    })
-    @MaxLength(16)
-    @IsOptional()
-    phoneNumber?: string;
-
-    @ApiPropertyOptional({
-        example: 'Laura Gomez',
-        description: 'username del estafador del incidente'
-    })
-    @IsString()
-    @MaxLength(32)
-    @IsOptional()
-    username?: string;
-
-    @ApiPropertyOptional({
-        example: 'tennisgratis@gmail.com',
-        description: 'correo del estafador del incidente'
-    })
-    @IsEmail()
-    @MaxLength(128)
-    @IsOptional()
-    email?: string;
-
-    @ApiPropertyOptional({
-        example: true,
-        description: 'Si el correo es anonimo o no'
-    })
+    @ApiProperty({ example: true, description: 'Si el correo es anonimo o no' })
     @IsBoolean()
-    @IsOptional()
-    anonymous?: boolean;
-
-    @ApiPropertyOptional({
-        example: '123132213123.png',
-        description: 'Id de la imagen obtenido por el file upload'
-    })
-    @IsOptional()
-    imageId?: string;
+    anonymous: boolean;
 }
